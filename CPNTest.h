1281dfe3507016115dff65bef4da09a0e04d0189
Ping Ngai Chung <pingngaichung@Pings-Mac-Pro.local>
2014-09-04 18:25:09 +0800

    added a test class

---
+++ CPNTest.h
@@ -0,0 +1,13 @@
+//
+//  CPNTest.h
+//  Git Test
+//
+//  Created by Ping Ngai Chung on 9/4/14.
+//  Copyright (c) 2014 CPN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPNTest : NSObject
+
+@end
---
+++ CPNTest.m
@@ -0,0 +1,13 @@
+//
+//  CPNTest.m
+//  Git Test
+//
+//  Created by Ping Ngai Chung on 9/4/14.
+//  Copyright (c) 2014 CPN. All rights reserved.
+//
+
+#import "CPNTest.h"
+
+@implementation CPNTest
+
+@end
