74b3250b1d61db492dcbd6ad520409ce9de9c666
Ping Ngai Chung <pingngaichung@Pings-Mac-Pro.local>
2014-09-04 18:01:18 +0800

    Initial Commit

---
+++ Git Test/CPNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPNAppDelegate.h
+//  Git Test
+//
+//  Created by Ping Ngai Chung on 9/4/14.
+//  Copyright (c) 2014 CPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CPNViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPNViewController.h
+//  Git Test
+//
+//  Created by Ping Ngai Chung on 9/4/14.
+//  Copyright (c) 2014 CPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPNViewController : UIViewController
+
+@end
